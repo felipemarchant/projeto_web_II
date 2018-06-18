34d275e82934394bdd9e63b1245671b8438f3c97
Felipe Marchant <felipemarchant@outlook.com>
2018-06-17 22:35:33 -0300

    Add voice teste

--- index.php
+++ index.php
@@ -7,81 +7,76 @@
       <title>Portal UNG</title>
    </head>
    <body id="portal">
-      <div class="container-fluid all">
+      <div  class="container-fluid all">
          <div class="row">
             <div class="slide-all col-sm-9 d-none d-sm-block"></div>
             <div class="menu-all col-sm">
-               <a href="#">
+               <a data-rvContext="UNG. Grupo Ser. fazendo o melhor por você" href="#">
                <img class="menu-logo img-fluid" src="skin/images/logo.png" alt="Logo" />
                </a>
-               <a href="#" class="menu-item">
+               <a data-rvContext="Graduação" href="#" class="menu-item">
                   <h2 class="title">Graduação</h2>
                   <p class="text">Presencial, semipresencial e a distância</p>
                </a>
                <hr/>
-               <a href="#" class="menu-item">
+               <a data-rvContext="Especialização e MBA" href="#" class="menu-item">
                   <h2 class="title">Especialização e MBA</h2>
                   <p class="text">Presencial e a distância</p>
                </a>
                <hr/>
-               <a href="#" class="menu-item">
+               <a data-rvContext="Mestrado e Doutorado" href="#" class="menu-item">
                   <h2 class="title">Mestrado e Doutorado</h2>
                </a>
                <hr/>
-               <a href="#" class="menu-item">
+               <a data-rvContext="Técnico" href="#" class="menu-item">
                   <h2 class="title">Técnico</h2>
                   <p class="text">Presencial e a distância</p>
                </a>
                <hr/>
-               <a href="#" class="menu-item">
+               <a data-rvContext="Congressos" href="#" class="menu-item">
                   <h2 class="title">Congressos</h2>
                   <p class="text">Presencial, semipresencial e a distância</p>
                </a>
                <hr/>
-               <a href="#" class="menu-item">
+               <a data-rvContext="Concurso" href="#" class="menu-item">
                   <h2 class="title">Concurso</h2>
                   <p class="text">A distância</p>
                </a>
                <hr/>
-               <a href="institucional.html" class="menu-item">
+               <a data-rvContext="Site Institucional" href="institucional.html" class="menu-item">
                   <h2 class="title" style="color:green;">Site Institucional</h2>
                </a>
                <hr/>
-               <a href="admin/login.php" class="menu-item">
+               <a data-rvContext="LOGIN" href="admin/login.php" class="menu-item">
                   <h2 class="title" style="color:red;">LOGIN</h2>
-                  <p class="text">A distância</p>
                </a>
                <hr/>
-               <a href="admin/" class="menu-item">
+               <a data-rvContext="Professor A distância" href="admin/" class="menu-item">
                   <h2 class="title">Professor</h2>
-                  <p class="text">A distância</p>
+                  <p class="text">a distância</p>
                </a>
                <hr/>
                <div class="groupfix">
 	               <div class="row-left">
-	                  <a class="btn-block" href="#">Idiomas</a>
-	                  <a class="btn-block" href="#">Profissionalizantes</a>
-                       <div class="col-sm small text-center">Bianca Liessi</div>
-		               <div class="col-sm small text-center">Felipe Marchant F. Soares</div>
-		               <div class="col-sm small text-center">Leandro M. Sanson</div>
+	                  <a data-rvContext="Idiomas" class="btn-block" href="#">Idiomas</a>
+	                  <a data-rvContext="Profissionalizantes" class="btn-block" href="#">Profissionalizantes</a>
+                       <div data-rvContext="Desenvolvido por: ianca Liessi" class="col-sm small text-center">Bianca Liessi</div>
+		               <div data-rvContext="Desenvolvido por: Felipe Marchant Fernandes Soares" class="col-sm small text-center">Felipe Marchant F. Soares</div>
+		               <div data-rvContext="Desenvolvido por: Leandro Sanson" class="col-sm small text-center">Leandro M. Sanson</div>
 	               </div>
 	               <div class="row-right">
-	                  <a class="btn-block" href="#">Ser educacional</a>
-	                  <a class="btn-block" href="#">CRA</a>
-                      <div class="col-sm small text-center">Henrique Cesar</div>
-              		  <div class="col-sm small text-center">Rodrigo Matsuura</div>
+	                  <a data-rvContext="Ser educacional" class="btn-block" href="#">Ser educacional</a>
+	                  <a data-rvContext="C.R.A" class="btn-block" href="#">CRA</a>
+                      <div data-rvContext="Desenvolvido por: Henrique Cesar" class="col-sm small text-center">Henrique Cesar</div>
+              		  <div data-rvContext="Desenvolvido por: Rodrigo Matsuura" class="col-sm small text-center">Rodrigo Matsuura</div>
 	               </div>
                </div>
                <div class="col-sm small text-center">Projeto Web de Acessibilidade</div>
             </div>
          </div>
       </div>
-      <?php 
-         /*require('app/core/config.php');
-         require('app/core/DB.php');
-         $db = DB::getConexao();
-         var_dump($db);*/
-         
-         ?>
+      <script src="lib/skin/js/jquery.min.js"></script>
+      <script src="lib/skin/js/responsivevoice.js"></script>
+      <script src="lib/skin/js/acessibilidade.js"></script>
    </body>
 </html>
